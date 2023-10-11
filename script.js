7fc6d85abcbf4694aec02b09d0d5ec460e9623c7
GDudut21 <girondudut92@gmail.com>
2023-10-12 03:43:51 +0800

    Added button in script.js
    Added the button and its function for navigation on website

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const Home = document.getElementsByClassName("HomePage")[0];
 const About = document.getElementsByClassName("AboutPage")[0];
+const Experience = document.getElementsByClassName("ExperiencePage")[0];
+const Project = document.getElementsByClassName("ProjectPage")[0];
+const Contacts = document.getElementsByClassName("ContactPage")[0];
 const homeButton = document.getElementById("Home");
 const aboutButton = document.getElementById("About");
-About.style.visibility = "hidden";
+const expButton = document.getElementById("Experience");
+const projectButton = document.getElementById("Project");
+const contactButton = document.getElementById("Contacts");
 Home.style.visibility = "visible";
-aboutButton.onclick = function hideCheck() {
+About.style.visibility = "hidden";
+Experience.style.visibility = "hidden";
+Project.style.visibility = "hidden";
+Contacts.style.visibility = "hidden";
+
+aboutButton.onclick = function about() {
   About.style.visibility = "visible";
   Home.style.visibility = "hidden";
+  Experience.style.visibility = "hidden";
+  Project.style.visibility = "hidden";
+  Contacts.style.visibility = "hidden";
 };
-homeButton.onclick = function hideCheck() {
-  About.style.visibility = "hidden";
+homeButton.onclick = function home() {
   Home.style.visibility = "visible";
+  About.style.visibility = "hidden";
+  Experience.style.visibility = "hidden";
+  Project.style.visibility = "hidden";
+  Contacts.style.visibility = "hidden";
+};
+expButton.onclick = function experience() {
+  Experience.style.visibility = "visible";
+  About.style.visibility = "hidden";
+  Home.style.visibility = "hidden";
+  Project.style.visibility = "hidden";
+  Contacts.style.visibility = "hidden";
+};
+projectButton.onclick = function project() {
+  Project.style.visibility = "visible";
+  About.style.visibility = "hidden";
+  Experience.style.visibility = "hidden";
+  Home.style.visibility = "hidden";
+  Contacts.style.visibility = "hidden";
+};
+contactButton.onclick = function contact() {
+  Contacts.style.visibility = "visible";
+  About.style.visibility = "hidden";
+  Experience.style.visibility = "hidden";
+  Home.style.visibility = "hidden";
+  Project.style.visibility = "hidden";
 };
